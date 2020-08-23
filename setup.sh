83319738031f9f21ba47de98e89d44ca87d6085e
diazonic <61781809+diazonic@users.noreply.github.com>
2020-08-23 11:01:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"ameer.valki@gmail.com\"\n\
+email = \"ameer.valki@gmail.com"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
+
